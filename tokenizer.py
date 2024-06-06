384ddd2ce5bb0645ee147240a88503914a6505d0
miyagawa-y-k <miyagawa-k@cs.st.kumamoto-u.ac.jp>
2024-06-06 15:02:12 +0900

    get_batch, train_test_splitの修正

--- tokenizer.py
+++ tokenizer.py
@@ -2,52 +2,59 @@ import torch
 import torch.nn as nn
 from torch.nn import functional as F
 
-#固定値
-batch_size = 64
-block_size = 256
-max_iters = 5000
-eval_interval = 500
-learning_rate = 3e-4
-device = 'cuda' if torch.cuda.is_available() else 'cpu'
-eval_iters = 200
-n_embd = 384
-n_head = 6
-n_layer = 6
-dropout = 0.2
+class Tokenizer():
+    def __init__(self):
+        self.chars = set()
+        self.char2int = {}
+        self.int2char = {}
+        self.train_data = []
+        self.val_data = []
+        #固定値
+        self.batch_size = 64
+        self.block_size = 256
+        self.max_iters = 5000
+        self.eval_interval = 500
+        self.learning_rate = 3e-4
+        self.device = 'cuda:0' if torch.cuda.is_available() else 'cpu'
+        self.eval_iters = 200
+        self.n_embd = 384
+        self.n_head = 6
+        self.n_layer = 6
+        self.dropout = 0.2
 
-def set_list(text):
-    #受け取ったテキストをsetに入れてすべての文字でソートする
-    chars = sorted(list(set(text)))
-    #文字とそれに対応したidを相互に変換する辞書の作成
-    char2int = {ch : i for i, ch in enumerate(chars)}
-    int2char = {i : ch for i, ch in enumerate(chars)}
-    return char2int, int2char
+    def set_list(self, text):
+        #受け取ったテキストをsetに入れてすべての文字でソートする
+        self.chars = sorted(list(set(text)))
+        #文字とそれに対応したidを相互に変換する辞書の作成
+        self.char2int = {ch : i for i, ch in enumerate(self.chars)}
+        self.int2char = {i : ch for i, ch in enumerate(self.chars)}
 
-def encoder(text, char2tint):
-    #encoder：文字列を数値列に変換
-    return list(char2tint[b] for b in text)
+    def encoder(self, text):
+        #encoder：文字列を数値列に変換
+        return list(self.char2int[b] for b in text)
 
-def decoder(tokens, int2char):
-    #受け取ったものがlist型ではない場合は変換
-    if type(tokens) != list:
-        tokens = tokens.tolist()
-    #受け取った系列を文字に戻す
-    return ''.join([int2char[b] for b in tokens])
+    def decoder(self, tokens):
+        #受け取ったものがlist型ではない場合は変換
+        if type(tokens) != list:
+            tokens = tokens.tolist()
+        #受け取った系列を文字に戻す
+        return ''.join([self.int2char[b] for b in tokens])
 
-def train_test_split(text, char2int):
-    #入力データを訓練データとテストデータに分割
-    data = torch.tensor(encoder(text, char2int), dtype=torch.long)
-    n = int(0.9 * len(data))
-    train_data = data[:n]
-    valid_data = data[n:]
-    return train_data, valid_data
+    def train_test_split(self, text):
+        #入力データを訓練データとテストデータに分割
+        data = torch.tensor(self.encoder(text), dtype=torch.long)
+        n = int(0.9 * len(data))
+        self.train_data = data[:n]
+        self.val_data = data[n:]
+        return self.train_data, self.val_data
 
-def get_batch(divided_data):
-    ix = torch.randint(len(divided_data) - block_size, (batch_size, ))
-    x = torch.stack([divided_data[i: i+block_size] for i in ix])
-    y = torch.stack([divided_data[i+1: i+block_size+1] for i in ix])
-    x, y = x.to(device), y.to(device)
-    return x, y
+    def get_batch(self, split):
+        data = self.train_data if split == 'train' else self.val_data
+        ix = torch.randint(len(data) - self.block_size, (self.batch_size, ))
+        x = torch.stack([data[i: i+self.block_size] for i in ix])
+        y = torch.stack([data[i+1: i+self.block_size+1] for i in ix])
+        x, y = x.to(self.device), y.to(self.device)
+        return x, y
     
     
     
